4439785171a33d9c610018f89157be801aa8bea4
ilyeshr2 <128542440+ilyeshr2@users.noreply.github.com>
2024-07-30 18:42:11 -0400

    Update HomePage.jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -68,38 +68,23 @@ export default function HomePage(props) {
 
 
   return (
-    <main className="  text-center flex-1 p-4 flex flex-col gap-3 sm:gp-4 justify-center pb-20">
-        <h1 className='font-semibold text-5xl sm:text-6xl md:text-7xl'>
-            Free
-            <span className='text-blue-400 bold'>
-                Scribe
-            </span>
-        </h1>
-        <h3 className='font-medium md:text-lg'>
-            Recorde
-            <span className='text-blue-400'>
-                &rarr;
-            </span>
-            Transcribe
-            <span className='text-blue-400'>
-                &rarr;
-            </span>
-            Translate
-        </h3>
-        <button onClick={recordingStatus==='recording' ? stopRecording : startRecording} className='flex items-center text-base justify-between gap-4 mx-auto w-72 max-w-full my-4 specialBtn px-4 py-2 rounded-xl'> 
-            <p className='text-blue-400'>{recordingStatus === 'inactive'? "Recorde" : `Stop recording`}</p>
-            <div className='flex items-center gap-2'>
-                {duration>0 && <p className='text-sm'>{duration}s</p>}
-            <i className={"fa-solid duration-200 fa-microphone" + (recordingStatus=== 'recording' ? 'text-rose-300' : "")}></i>
-            </div>
-        </button>
-        <p className='text-base'>Or <label
-         className='text-blue-400 cursor-pointer hover:text-blue-600 duration-200'>
-            upload <input onChange={(e)=>{
-                const tempFile=e.target.files[0]
+    <main className='flex-1  p-4 flex flex-col gap-3 text-center sm:gap-4  justify-center pb-20'>
+    <h1 className='font-semibold text-5xl sm:text-6xl md:text-7xl'>Free<span className='text-blue-400 bold'>Scribe</span></h1>
+    <h3 className='font-medium md:text-lg'>Record <span className='text-blue-400'>&rarr;</span> Transcribe <span className='text-blue-400'>&rarr;</span> Translate</h3>
+            <button onClick={recordingStatus === 'recording' ? stopRecording : startRecording} className='flex specialBtn px-4 py-2 rounded-xl items-center text-base justify-between gap-4 mx-auto w-72 max-w-full my-4'>
+                <p className='text-blue-400'>{recordingStatus === 'inactive' ? 'Record' : `Stop recording`}</p>
+                <div className='flex items-center gap-2'>
+                     {duration !== 0 && (
+                        <p className='text-sm'>{duration}s</p>
+                    )} 
+                    <i className={"fa-solid duration-200 fa-microphone " + (recordingStatus === 'recording' ? ' text-rose-300' : "")}></i>
+                </div>
+            </button>
+            <p className='text-base'>Or <label className='text-blue-400 cursor-pointer hover:text-blue-600 duration-200'>upload <input onChange={(e) => {
+                const tempFile = e.target.files[0]
                 setFile(tempFile)
-        }} className='hidden' type="file" accept='.mp3,.wave'/></label>a mp3 file</p>
-        <p className='italic text-slate-400'>Free now free forever</p>
-    </main>
+            }} className='hidden' type='file' accept='.mp3,.wave' /></label> a mp3 file</p>
+            <p className='italic text-slate-400'>Free now free forever</p>
+        </main>
   )
 }
